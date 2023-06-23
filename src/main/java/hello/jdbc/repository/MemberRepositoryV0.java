c00f9837656364802d80160f1f72c8cf2e353b88
mjh9401 <mjh9401@naver.com>
2023-06-23 23:45:13 +0900

    JDBC 조회, 수정, 삭제

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManger 사용
@@ -12,6 +13,9 @@ import java.sql.*;
 @Slf4j
 public class MemberRepositoryV0 {
 
+    /**
+     * 회원 가입
+     */
     public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id,money) values (?,?)";
 
@@ -34,7 +38,101 @@ public class MemberRepositoryV0 {
 
     }
 
+    /**
+     * 회원 조회
+     * @param memberId
+     * @return 조회 결과
+     * @throws SQLException
+     */
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+        Connection con= null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1,memberId);
+            rs = pstmt.executeQuery();
+            if(rs.next()){
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId="+memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("db error",e);
+            throw e;
+        } finally {
+            close(con,pstmt,rs);
+        }
+    }
+
+    /**
+     * 회원 정보 수정
+     * @param memberId
+     * @param money
+     * @throws SQLException
+     */
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id = ?";
+
+        Connection con =null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1,money);
+            pstmt.setString(2,memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize={}",resultSize);
+        } catch (SQLException e) {
+            log.error("db error",e);
+            throw e;
+        }finally {
+            close(con,pstmt,null);
+        }
+    }
+
+    /**
+     * 회원정보 삭제
+     * @param memberId
+     * @throws SQLException
+     */
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id = ?";
+
+        Connection con =null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1,memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize={}",resultSize);
+        } catch (SQLException e) {
+            log.error("db error",e);
+            throw e;
+        }finally {
+            close(con,pstmt,null);
+        }
+    }
+
+
     private void close(Connection con, Statement stmt, ResultSet rs){
+        if(rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error",e);
+            }
+        }
+
         if(stmt != null){
             try {
                 stmt.close();
@@ -51,13 +149,6 @@ public class MemberRepositoryV0 {
             }
         }
 
-        if(rs != null){
-            try {
-                rs.close();
-            } catch (SQLException e) {
-                log.info("error",e);
-            }
-        }
     }
 
     private Connection getConnection() {
